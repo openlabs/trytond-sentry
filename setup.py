b713f6e248d19ae9e8487e0b9c0f2fe13d0553ff
Shalabh Aggarwal <shalabh.aggarwal@openlabs.co.in>
2012-08-07 18:45:02 +0530

    Initial module for Version 2.4

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+    setup
+
+
+    :copyright: (c) 2012 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from setuptools import setup
+
+setup(name='trytond_sentry',
+    version='2.4.0.1',
+    description=__doc__,
+    author="Openlabs Technologies & Consulting (P) Limited",
+    author_email="info@openlabs.co.in",
+    url="http://www.openlabs.co.in",
+    package_dir={'trytond_sentry': '.'},
+    packages=[
+        'trytond_sentry',
+    ],
+    scripts=[
+        'bin/trytond_sentry',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Office/Business',
+    ],
+    license='GPL-3',
+    install_requires=[
+        "trytond>=2.4,<2.5",
+        "raven",
+    ],
+    zip_safe=False,
+)
